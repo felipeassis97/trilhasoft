0f4954a47ff8ec7558974acb7e89a1ca6f89f0a2
felipeassis97 <felipeassis97@gmail.com>
2021-02-24 18:34:57 -0300

    Primeiras Classes

--- semana_1/projAlura/byteBank/lib/main.dart
+++ semana_1/projAlura/byteBank/lib/main.dart
@@ -1,9 +1,11 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          body: listaTransferencia(),
+          body: listaTransferencias(),
           appBar: AppBar(
             title: Text('Transferências'),
           ),
@@ -14,26 +16,33 @@ void main() => runApp(
       ),
     );
 
-class listaTransferencia extends StatelessWidget {
+class listaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('66.00'),
-            subtitle: Text('12538-45'),
-          ),
-        ),
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('250.00'),
-            subtitle: Text('12538-45'),
-          ),
-        ),
+        itemTransferencia('984.00', '15845-45'),
+        itemTransferencia('114.00', '08524-00'),
+        itemTransferencia('289.00', '65987-24'),
       ],
+    );
+  }
+}
+
+class itemTransferencia extends StatelessWidget {
+  final String valor;
+  final String numeroConta;
+
+  itemTransferencia(this.valor, this.numeroConta);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(valor),
+        subtitle: Text(numeroConta),
+      ),
     );
   }
 }
