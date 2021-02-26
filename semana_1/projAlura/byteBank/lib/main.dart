1639687b0de0e4b87517089b3b05fedad66facf9
felipeassis97 <felipeassis97@gmail.com>
2021-02-26 13:14:58 -0300

    Refatoração Projeto

--- semana_1/projAlura/byteBank/lib/main.dart
+++ semana_1/projAlura/byteBank/lib/main.dart
@@ -1,44 +1,57 @@
 // ignore: avoid_web_libraries_in_flutter
-import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          body: listaTransferencias(),
+          body: ListaTransferencias(),
           appBar: AppBar(
             title: Text('Transferências'),
           ),
           floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Icon(Icons.add_box_sharp);
+            },
             child: Icon(Icons.add),
           ),
         ),
       ),
     );
 
-class listaTransferencias extends StatelessWidget {
+class ListaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[Text('1213')],
+      children: <Widget>[
+        ItemTransferencia(Transferencia(213.00, 1457659)),
+        ItemTransferencia(Transferencia(234.99, 987876)),
+        ItemTransferencia(Transferencia(23.34, 2344432))
+      ],
     );
   }
 }
 
-class itemTransferencia extends StatelessWidget {
-  final String valor;
-  final String numeroConta;
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
 
-  itemTransferencia(this.valor, this.numeroConta);
+  ItemTransferencia(this._transferencia);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
         leading: Icon(Icons.monetization_on),
-        title: Text(valor),
-        subtitle: Text(numeroConta),
+        title: Text(_transferencia._valor.toString()),
+        subtitle: Text(_transferencia._numeroConta.toString()),
       ),
     );
   }
+}
+
+class Transferencia {
+  final double _valor;
+  final int _numeroConta;
+
+  Transferencia(this._valor, this._numeroConta);
 }
