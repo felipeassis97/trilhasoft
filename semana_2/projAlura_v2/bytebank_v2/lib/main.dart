a5a8b09fa5b367da818474de7d40536802650b46
felipeassis97 <felipeassis97@gmail.com>
2021-03-06 09:55:48 -0300

    Inicio Projeto V2

---
+++ semana_2/projAlura_v2/bytebank_v2/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'screens/transferencia/transferencia.dart';
+
+void main() => runApp(BytebankAppV2());
+
+class BytebankAppV2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: ListaTransferencias(),
+    );
+  }
+}
